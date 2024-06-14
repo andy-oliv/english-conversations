c009a86f83142ecc1bee197f3c74c34ef090e033
Andrew Pacheco <andrew.pacheco@outlook.com>
2024-06-14 15:30:17 -0300

    revisão da responsividade, alteração do botão pageUp com smooth scroll ao invés de recarregamento da página

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ let questions = document.querySelectorAll(".question")
 const whatsAppLogo = document.querySelector(".whatsLogo")
 let messageWrapper = document.querySelector(".messageWrapper")
 let pageUpBtn = document.querySelector(".pageUp")
-
+let mobileMenuBtn = document.getElementById("mobileIcon")
+let mobileMenu = document.querySelector(".mobileMenu")
+let closeMenuBtn = document.getElementById("closeBtn")
 
 questions.forEach((question)=>{
     question.addEventListener("click", ()=>{
@@ -32,4 +34,16 @@ window.addEventListener("scroll", ()=>{
         pageUpBtn.style.visibility = "hidden"
         pageUpBtn.style.opacity = 0
     }
+})
+
+mobileMenuBtn.addEventListener("click", ()=>{
+    mobileMenu.classList.toggle("menuClicked")
+})
+
+closeMenuBtn.addEventListener("click", ()=>{
+    mobileMenu.classList.toggle("menuClicked")
+})
+
+pageUpBtn.addEventListener("click", ()=>{
+   window.scrollTo(top)
 })
